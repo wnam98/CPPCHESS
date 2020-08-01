23134ce6da8b9e416b78d4e22d0e4653b48ec6b6
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-08-01 13:42:35 +0900

    chess board background and initialized pieces

--- main.cpp
+++ main.cpp
@@ -4,14 +4,55 @@
 #include "Connector.hpp"
 using namespace sf;
 
+int size = 56;
+Sprite f[32];
+
+int board [8][8] =
+        {-1, -2, -3, -4, -5, -3, -2, -1,
+         -6, -6, -6, -6, -6, -6, -6, -6,
+          0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0,
+          6, 6, 6, 6, 6, 6, 6, 6,
+          1, 2, 3, 4, 5, 3, 2, 1,};
+
+void loadPosition()
+{
+    int k = 0;
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            int n = board[i][j];
+            if(!n) continue;
+            int x = abs(n) - 1;
+            int y = n > 0 ? 1 : 0;
+            f[k].setTextureRect(IntRect(size * x, size * y, size, size));
+            f[k].setPosition(size * j, size * i);
+            k++;
+        }
+    }
+}
+
 int main()
 {
-    RenderWindow window(VideoMode(550, 550), "Chess");
+    RenderWindow window(VideoMode(453, 453), "Chess");
     Texture tx1;
+    Texture tx2;
     tx1.loadFromFile("images/figures.png");
+    tx2.loadFromFile("images/board.png");
+
     Sprite s(tx1);
+    Sprite sBoard(tx2);
+
+    for(int i = 0; i < 32; i++) f[i].setTexture(tx1);
+
+    loadPosition();
+
     bool move = false;
     float dx = 0, dy = 0;
+    int n = 0;
 
     while(window.isOpen())
     {
@@ -26,11 +67,12 @@ int main()
             {
                 if (e.key.code == Mouse::Left)
                 {
-                    if (s.getGlobalBounds().contains(pos.x, pos.y))
+                    for(int i = 0; i < 32; i++)
+                    if (f[i].getGlobalBounds().contains(pos.x, pos.y))
                     {
-                        move = true;
-                        dx = pos.x - s.getPosition().x;
-                        dy = pos.y - s.getPosition().y;
+                        move = true; n = i;
+                        dx = pos.x - f[i].getPosition().x;
+                        dy = pos.y - f[i].getPosition().y;
                     }
                 }
             }
@@ -44,9 +86,10 @@ int main()
             }
         }
 
-        if(move) s.setPosition(pos.x - dx, pos.y - dy);
+        if(move) f[n].setPosition(pos.x - dx, pos.y - dy);
         window.clear();
-        window.draw(s);
+        window.draw(sBoard);
+        for(int i = 0; i < 32; i++) window.draw(f[i]);
         window.display();
     }
 
