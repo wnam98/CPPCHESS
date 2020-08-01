f7cacc45a684a1c6207eb7fab30ca9e58bb8ebdf
Walter Nam <wkn2@case.edu>
2020-08-01 09:54:48 +0900

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/CPPChess.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPChess.dir/main.cpp.obj"
+  "CPPChess.exe"
+  "CPPChess.exe.manifest"
+  "CPPChess.pdb"
+  "libCPPChess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPChess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
